2c33d43be75c366f3157f06017a22d302d80eced
YvesPhilippot <yvessnake@hotmail.com>
2018-12-20 21:54:00 +0100

    Update index.js

--- redux-test/src/reducers/index.js
+++ redux-test/src/reducers/index.js
@@ -8,8 +8,8 @@ export default combineReducers({
 })
 
 
-/* les réducers permettent de définir comment de l'état de l'application va changer
-On prends une action qui décrire ce qu'il se passe mais pas comment
+/* les réducers permettent de définir comment l'état de l'application va changer
+On prends une action qui va décrire ce qu'il se passe mais pas comment
 
 Les reducers doivent éviter les choses compliquées. Pas d'appel de Web API, pas de calcule utilisant des fonctiont comme la date ou encore du random. 
 
